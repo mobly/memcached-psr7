92f4719caf29317685a55db500dd28e02f6ba86e
Rodrigo Lacerda Pereira <rodrigo.pereira@mobly.com.br>
2016-02-23 11:34:40 -0300

    Add configuration

---
+++ src/MemcachedConfiguration.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mobly\Cache\Adapter\Memcached;
+
+use Mobly\Cache\CacheAdapterConfiguration;
+
+/**
+ * Class CacheAdapterConfiguration
+ * @package Mobly\Cache
+ */
+class MemcachedConfiguration extends CacheAdapterConfiguration
+{
+
+}
